4e3f5c4719526b90820912b6fa1abaa16f3a07ac
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-12-14 21:45:48 +0530

    added-build-script

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,6 +1,19 @@
-import app from "./app.js";
+import express from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import { authRouter } from "./auth/index.js";
+import { shopRouter } from "./shop/index.js";
+import { sweetRouter } from "./sweets/index.js";
 
 const port = process.env.BACKEND_PORT || 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());    
+
+app.use('/api/auth', authRouter);
+app.use('/api/shop', shopRouter);
+app.use('/api/sweet', sweetRouter);
 
 app.listen(port, () => {
     console.log(`Your app is listening on ${port}`);
